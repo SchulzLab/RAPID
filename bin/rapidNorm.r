d3143ef9c8bb4e766a5acfa974b26b2ffb9bf9b7
Sivarajan Karunanithi <skarunan@srv-13-14.mpi-inf.mpg.de>
2017-07-05 15:38:23 +0200

    BugFix: NA/Inf issues and modified length specific counts to be merged by region names to avoid mix up in ordering

--- bin/rapidNorm.r
+++ bin/rapidNorm.r
@@ -68,6 +68,7 @@ normalizeEntries <- function(Stats,oldTotal,adjustForBackground=TRUE,normalizeBy
       newList[[i]]$asreads_norm=round(newList[[i]]$antisenseReads/sizes[i],digits = 0)
       newList[[i]]$asratio_norm=round(newList[[i]]$asreads_norm/newList[[i]]$reads_norm,digits=2)
       newList[[i]]$asratio_norm[is.na(newList[[i]]$asratio_norm)]=0
+      newList[[i]]$asratio_norm[is.infinite(newList[[i]]$asratio_norm)]=0
       if(normalizeByLength){
         newList[[i]]$reads_avg=round( (newList[[i]]$reads_norm/newList[[i]]$lens),digits=3)
       }
@@ -84,6 +85,7 @@ normalizeEntries <- function(Stats,oldTotal,adjustForBackground=TRUE,normalizeBy
       newList[[i]]$asreads_norm=round(newList[[i]]$antisenseReads*(Max/Total[[i]]),digits=0)
       newList[[i]]$asratio_norm=round(newList[[i]]$asreads_norm/newList[[i]]$reads_norm,digits=2)
       newList[[i]]$asratio_norm[is.na(newList[[i]]$asratio_norm)]=0
+      newList[[i]]$asratio_norm[is.infinite(newList[[i]]$asratio_norm)]=0
       if(normalizeByLength){
         newList[[i]]$reads_avg=round( (newList[[i]]$reads_norm/newList[[i]]$lens),digits=3)
       }
--- bin/rapidStats.r
+++ bin/rapidStats.r
@@ -59,10 +59,8 @@ rmax=range(tt$V2)[2]+1
 
 ##create datastructure that counts reads per length per region and strand
 allNames=annotations$label[!duplicated(annotations$label)]
-#names=paste(c(17:25,17:25),c(rep("+",9),rep("-",9)),sep="")
 names=paste(c((rmin+1):(rmax-1), (rmin+1):(rmax-1)), c(rep("+", rmax-rmin-1), rep("-", rmax-rmin-1)), sep = "")
-allCounts=rep(0,length(names))
-#names(allCounts)=names
+allCounts=rep(0,length(names)+1)
 for(name in allNames){
   counts=rep(0,length(names))
   subb=subset(tt,V1 == name)
@@ -74,14 +72,13 @@ for(name in allNames){
     subb$LS=sub('t$','-',subb$LS)
   }
   for (i in 1:length(names)){
-    #counts[i]=length(grep(names[i],tt$LS,fixed=T))
     counts[i]=nrow(subset(subb,LS == names[i]))
   }
-  allCounts=rbind(allCounts,counts)
-  
+  allCounts=rbind(allCounts,c(name,counts))
 }
 #remove first dummy row
 allCounts=allCounts[-1,]
+
 #stats function returns line of stats for each matrix of alignments
 computeStats <- function(subMatrix,header){
   reads=nrow(subMatrix)
@@ -95,17 +92,21 @@ computeStats <- function(subMatrix,header){
   Cminus=nrow(subset(subMatrix,V4 == "-" & V5 == "C"))
   Gminus=nrow(subset(subMatrix,V4 == "-" & V5 == "G"))
   Tminus=nrow(subset(subMatrix,V4 == "-" & V5 == "T"))
-  Aratio=round(Aminus/(Aplus+Aminus),2)
-  Cratio=round(Cminus/(Cplus+Cminus),2)
-  Gratio=round(Gminus/(Gplus+Gminus),2)
-  Tratio=round(Tminus/(Tplus+Tminus),2)
   if(reads==0){
     ASratio=0
     MODratio=0
+    Aratio=0
+    Cratio=0
+    Gratio=0
+    Tratio=0
   }
   else {
     ASratio=round(stranded/reads,2)
     MODratio=round(mod/reads,2)
+    Aratio=round(Aminus/(Aplus+Aminus),2)
+    Cratio=round(Cminus/(Cplus+Cminus),2)
+    Gratio=round(Gminus/(Gplus+Gminus),2)
+    Tratio=round(Tminus/(Tplus+Tminus),2)
   }
   return(as.character(c(header,reads,mod,MODratio,stranded,ASratio,Aplus,Cplus,Gplus,Tplus,Aminus,Cminus,Gminus,Tminus,Aratio,Cratio,Gratio,Tratio)))
 }
@@ -122,17 +123,21 @@ computeStatsNeg <- function(subMatrix,header){
   Cplus=nrow(subset(subMatrix,V4 == "-" & V5 == "C"))
   Gplus=nrow(subset(subMatrix,V4 == "-" & V5 == "G"))
   Tplus=nrow(subset(subMatrix,V4 == "-" & V5 == "T"))
-  Aratio=round(Aminus/(Aplus+Aminus),2)
-  Cratio=round(Cminus/(Cplus+Cminus),2)
-  Gratio=round(Gminus/(Gplus+Gminus),2)
-  Tratio=round(Tminus/(Tplus+Tminus),2)
   if(reads==0){
     ASratio=0
     MODratio=0
+    Aratio=0
+    Cratio=0
+    Gratio=0
+    Tratio=0
   }
   else {
     ASratio=round(stranded/reads,2)
     MODratio=round(mod/reads,2)
+    Aratio=round(Aminus/(Aplus+Aminus),2)
+    Cratio=round(Cminus/(Cplus+Cminus),2)
+    Gratio=round(Gminus/(Gplus+Gminus),2)
+    Tratio=round(Tminus/(Tplus+Tminus),2)
   }
   return(as.character(c(header,reads,mod,MODratio,stranded,ASratio,Aplus,Cplus,Gplus,Tplus,Aminus,Cminus,Gminus,Tminus,Aratio,Cratio,Gratio,Tratio)))
 }
@@ -160,6 +165,17 @@ thead=c("region","reads","modified","MODratio","antisenseReads","ASratio","A+","
 Stats=rbind(thead,as.data.frame(Stats2))
 
 #add the header line of allCounts to the matrix to have the same nrow then Stats
-allCounts = rbind(names,allCounts)
-Stats=cbind(Stats,allCounts)
-write.table(Stats,outStats,quote=F,col.names=F,row.names=F,sep=" ")
+#allCounts = rbind(names,allCounts)
+allCounts=rbind(c("region",names),allCounts)
+
+dfLenCounts=as.data.frame(allCounts)
+colnames(dfLenCounts)=allCounts[1,]
+dfLenCounts=dfLenCounts[-1,]
+
+colnames(Stats)=thead
+Stats=Stats[-1,]
+
+mergedStats=merge(Stats,dfLenCounts,by="region")
+
+#Stats=cbind(Stats,allCounts)
+write.table(mergedStats,outStats,quote=F,col.names=T,row.names=F,sep=" ")
