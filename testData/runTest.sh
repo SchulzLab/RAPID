86f9b02288027624f4be62281a3461c8284cd32b
Sivarajan Karunanithi <skarunan@srv-13-12.mpi-inf.mpg.de>
2018-03-19 15:21:20 +0100

    Updating testdata, and removing old testdata related information

--- testData/runTest.sh
+++ testData/runTest.sh
@@ -4,6 +4,7 @@
 ## with two regions in the Regions.bed
 ## After the first part of the pipeline (rapidStats) is complete, an example comparative
 ## analysis (with three times the same data) is run to verify the different outputs through rapidVis.r
+## rapidVis.r test run for normalization output is excluded, as the quality plots (like MDS) would fail because of identical data.
 ##
 ## Note that bedtools2, bowtie2, samtools (for bam support) and the RAPID bin folder should be in your PATH
 ## otherwise uncomment the lines below and add the paths.
@@ -15,17 +16,12 @@
 #PATH=$PATH:/  #RAPID/bin/
 
 #rapid=/home/software/RAPID-master/bin/
-rapid=/MMCI/MS/smallRNA-1/work/paramecium/softwares/githubrepos/RAPID/bin/
 
 #first create a rapid analysis of a small data set with 1 gene and 2 regions
-bash ${rapid}rapidStats.sh --file=test.fastq.gz --out=TestRapid/ --remove=yes --annot=Regions_strand.bed --index=./GeneIndex --rapid=$rapid
-bash ${rapid}rapidStats.sh --file=test.bam -ft=BAM --out=TestRapidBAM/ --remove=yes --annot=Regions_strand.bed --index=./GeneIndex --rapid=$rapid
-bash ${rapid}rapidStats.sh --file=test.sam -ft=SAM --out=TestRapidSAM/ --remove=yes --annot=Regions_strand.bed --index=./GeneIndex --rapid=$rapid
+bash ${rapid}rapidStats.sh --file=test.fastq.gz --out=TestRapid/ --remove=yes --annot=Regions.bed --index=./GeneIndex --rapid=$rapid
 
 #second use the config file test.config to generate a toy comparative analysis
-bash ${rapid}rapidNorm.sh --conf=test_strand.config --out=TestCompareStrand/ --annot=Regions_strand.bed  --rapid=$rapid
+bash ${rapid}rapidNorm.sh --conf=test.config --out=TestCompare/ --annot=Regions.bed  --rapid=$rapid
 
 #third run the rapidVis pipeline to generate the visualizations for both rapidStats and rapidNorm
-R3script ${rapid}rapidVis.r stats ./TestRapid/Regions_strand/ ./Regions_strand.bed $rapid
-R3script ${rapid}rapidVis.r stats ./TestRapidBAM/Regions_strand/ ./Regions_strand.bed $rapid
-R3script ${rapid}rapidVis.r stats ./TestRapidSAM/Regions_strand/ ./Regions_strand.bed $rapid
+Rscript ${rapid}rapidVis.r stats ./TestRapid/Regions/ ./Regions.bed $rapid
